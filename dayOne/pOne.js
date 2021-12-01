35094146d0f4c22fff2da07b52a09ad3897f6bc9
Cian Ormond <cow57xd@gmail.com>
2021-12-01 01:03:20 -0800

    Day one puzzle one

---
+++ dayOne/pOne.js
@@ -0,0 +1,25 @@
+var previousNumber = 0;
+var currentIndex = 0;
+
+var incrementCount = 0;
+
+var numbers = [];
+
+const fs = require('fs');
+
+fs.readFile('input', (err, data) => {
+    if (err) throw err;
+    numbers = data.toString().split('\n');
+    numbers.pop();
+    previousNumber = parseInt(numbers[0]);
+    for (var i = 1; i < numbers.length; i++) {
+        currentIndex = i;
+        var currentNumber = parseInt(numbers[i]);
+        if (currentNumber > previousNumber) {
+            incrementCount++;
+        }
+        previousNumber = currentNumber;
+    }
+    
+    console.log(incrementCount);
+});
